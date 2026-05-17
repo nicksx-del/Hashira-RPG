9867c7ef0b1913d3d673254ad0bfa05e3658a4c6
Nicolas S <nicolas9silveira@gmail.com>
2026-05-17 20:48:39 -0300

    Revert "Add web frontend (Vite + React + TypeScript)"
    This reverts commit 14513cd296fe89b137af83748972e5882951e2d7.

--- web/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { RouterProvider } from 'react-router-dom'
-import { appRouter } from './app/router'
-
-function App() {
-  return <RouterProvider router={appRouter} />
-}
-
-export default App
